b2691775fa413e0ca60b1df7d12bb2b24ed01165
Jason Aravanis <j.aravanis@icloud.com>
2021-10-06 19:14:38 +1100

    Add main article section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
     extend: {
       colors: {
         "ns-blue": "rgb(78, 177, 224)",
+        "ns-button-blue": "rgb(0, 85, 135)",
         "ns-gray": "rgb(142, 142, 142)",
+        "ns-beige": "rgb(244,244,243)",
+        "fb-blue": "rgb(59, 89, 152)",
+        "twitter-blue": "rgb(91, 180, 240)",
+        "whatsapp-green": "rgb(124, 205, 123)",
+        "linkedIn-blue": "rgb(72, 149, 192)",
+        "reddit-orange": "rgb(255, 87, 0)",
       },
       fontFamily: {
         titillium: ["Titillium Web", "Helvetica", "Trebuchet MS", "Arial"],
